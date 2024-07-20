9c2604b6b0bdf35da90ddb016443510eeae0fa50
jayak <kumarijaya127@gmail.com>
2024-07-20 15:15:00 +0530

    added odd numbers print

---
+++ Odd.java
@@ -0,0 +1,12 @@
+class Odd{
+    public static void main(String[] args){
+        int i=0;
+        while(i<=50){
+            if(i%2!=0){
+             System.out.println("Odd numbers are "+ i);
+            }
+            i++;
+            
+        }
+    }
+}
