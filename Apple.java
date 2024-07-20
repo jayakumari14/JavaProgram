f928a8a9aae759f7dd8a9fa75cd8af3c794e9aa3
jayak <kumarijaya127@gmail.com>
2024-07-20 15:06:16 +0530

    added even numbers print

--- Apple.java
+++ Apple.java
@@ -3,7 +3,7 @@ public static void main(String[] args)
 {
 int i=1;
 while(i<=10){
-System.out.println("2 * "+ i + " " + "=" + " " +  i*2);
+System.out.println("3 * "+ i + " " + "=" + " " +  i*3);
 i++;
 }
 }
---
+++ Even.java
@@ -0,0 +1,12 @@
+class Even{
+    public static void main(String[] args){
+        int i=0;
+        while(i<=50){
+            if(i%2==0){
+             System.out.println("even numbers are "+ i);
+            }
+            i++;
+            
+        }
+    }
+}
